016b0594baec3b617e23fcaf6ffcc803cc07f7e5
an040207 <timmykmine@outlook.com>
2022-04-05 22:59:29 +0900

    first commit

---
+++ project.py
@@ -0,0 +1,11 @@
+import turtle
+
+a = turtle.Turtle()
+a.color("green")
+a.shape("blank")
+a.speed("fastest")
+i = 0   #i가 커질수록 무늬의 간격이 커진다
+while True:
+    a.forward(i)
+    a.right(91)
+    i += 4
