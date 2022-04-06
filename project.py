4e69ef02df6c5af3da8b8e5155a9d9df5e523a2a
an040207 <timmykmine@outlook.com>
2022-04-06 17:51:28 +0900

    modified: project.py

--- project.py
+++ project.py
@@ -1,11 +1,17 @@
 import turtle
+import random
 
 a = turtle.Turtle()
 a.color("green")
 a.shape("blank")
 a.speed(0)
-i = 0   #i가 커질수록 무늬의 간격이 커진다
+i = 0 #변수 i 선언
+j = 0 #변수 j 선언
 while True:
-    a.forward(i)
-    a.right(91)
-    i += 0.5
+    i = random.randrange(-360,361)
+    j = random.randrange(-360,361)
+    a.forward(abs(i))
+    a.right(i)
+    a.circle(i)
+    a.circle(j)
+    a.goto(i,j)
