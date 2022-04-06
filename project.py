eab61e80c25a953e6e979eb1a6d35b37e0b25a95
an040207 <timmykmine@outlook.com>
2022-04-06 17:30:50 +0900

    modified: project.py

--- project.py
+++ project.py
@@ -3,9 +3,9 @@ import turtle
 a = turtle.Turtle()
 a.color("green")
 a.shape("blank")
-a.speed("fastest")
+a.speed(0)
 i = 0   #i가 커질수록 무늬의 간격이 커진다
 while True:
     a.forward(i)
     a.right(91)
-    i += 1
+    i += 0.5
