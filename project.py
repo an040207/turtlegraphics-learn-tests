a341b852f98c428fe332ff5c12555b8537145974
an040207 <timmykmine@outlook.com>
2022-04-08 20:11:10 +0900

    modified: project.py

--- project.py
+++ project.py
@@ -9,8 +9,8 @@ def square(size):
     for _ in range(4):
         a.forward(size)
         a.left(90)
-i = 0 #변수 i 선언
-j = 0 #변수 j 선언
+i = 0
+j = 0
 while True:
     i = random.randrange(-360,361)
     j = random.randrange(-360,361)
