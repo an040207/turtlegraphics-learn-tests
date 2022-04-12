0e5b56989691061b14154b2a2c52bfcc461f969a
an040207 <timmykmine@outlook.com>
2022-04-12 13:47:19 +0900

    code style change

--- project.py
+++ project.py
@@ -2,24 +2,29 @@ import turtle
 import random
 
 turtle.colormode(255)
-turtle.bgcolor(0,0,0)
+
+turtle.bgcolor(0 ,0 ,0)
+
 a = turtle.Turtle()
+
 a.shape("blank")
 a.speed(0)
+
 def square(size):
     for _ in range(4):
         a.forward(size)
         a.left(90)
+
 while True:
-    i = random.randrange(-360,361)
-    j = random.randrange(-360,361)
-    ca = random.randrange(0,256)
-    cb = random.randrange(0,256)
-    cc = random.randrange(0,256)
-    a.color(ca,cb,cc)
+    i = random.randrange(-360 ,361)
+    j = random.randrange(-360 ,361)
+    ca = random.randrange(0 ,256)
+    cb = random.randrange(0 ,256)
+    cc = random.randrange(0 ,256)
+    a.color((ca, cb ,cc))
     a.forward(abs(i))
     a.right(i)
     a.circle(i)
     a.circle(j)
     square(i)
-    a.goto(i,j)
+    a.goto(i ,j)
